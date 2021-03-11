4b329fa5572bf204682c4bddfa8676357a7c3687
7g3p <stephenperrin4@live.ca>
2021-03-11 17:47:53 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Layout from './Components/layout.js';
+import SearchSubreddits from './search.js';
+import FavouritePosts from './favourites.js';
+import "tailwindcss/tailwind.css"
+import './App.css';
+
+function App() {
+
+  return (
+      <BrowserRouter>
+      <Layout>
+        <Switch>
+          <Route path="/favourites" component={FavouritePosts}/>
+          <Route path="/search" component={SearchSubreddits}/>
+        </Switch>
+        </Layout>
+      </BrowserRouter>
+  );
+}
+
+export default App;
